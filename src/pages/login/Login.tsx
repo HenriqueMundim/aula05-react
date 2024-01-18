050ed92ef07bc50b7281f3a8eca56e8ff6657fb1
Henrique Matos <henriquemundimmatos@gmail.com>
2024-01-18 18:38:09 -0300

    feat(login): login screen done

---
+++ src/pages/login/Login.tsx
@@ -0,0 +1,35 @@
+import { useRef } from 'react'
+import styles from './Login.module.css'
+import infinityIcon from '../../assets/Infinity.svg'
+
+function App() {
+  const emailRef = useRef<HTMLInputElement>(null)
+  const passwordRef = useRef<HTMLInputElement>(null)
+
+  const handleLogin = () => {
+    const email = emailRef.current?.value
+    const password = passwordRef.current?.value
+  }
+
+  return (
+    <main className={styles.container}>
+      <div className={styles.card}>
+        <div className={styles['card-header']}>
+          <img src={infinityIcon} alt="" />
+          <h1>Fazer Log In</h1>
+        </div>
+        <form action="">
+          <label htmlFor="email">Email</label>
+          <input type="email" ref={emailRef} id='email' />
+          <label htmlFor="password">Password</label>
+          <input type="password" ref={passwordRef} />
+          <span>Não possui uma conta?</span>
+          <button type="submit" onClick={handleLogin}>ENTRAR</button>
+        </form>
+      </div>
+    </main >
+  )
+
+}
+
+export default App
